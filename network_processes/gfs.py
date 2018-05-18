421ebfe37551d070d5c907a3302035a33c5909dc
PeterStAndrews <pm78@st-andrews.ac.uk>
2018-05-18 10:45:22 +0100

    Update gfs.py

--- network_processes/gfs.py
+++ network_processes/gfs.py
@@ -2,21 +2,21 @@
 #
 # Copyright (C) 2017 Peter Mann
 # 
-# This file is part of `Network-processes`, for epidemic network 
+# This file is part of `Network_processes`, for epidemic network 
 # analytical results using Python.
 #
-# `Network-processes` is free software: you can redistribute it and/or modify
+# `Network_processes` is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# `Network-processes` is distributed in the hope that it will be useful,
+# `Network_processes` is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with `Network-processes`. If not, see <http://www.gnu.org/licenses/gpl.html>.
+# along with `Network_processes`. If not, see <http://www.gnu.org/licenses/gpl.html>.
 
 
 from network_processes import *
