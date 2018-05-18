8e0a0a9d25072c2688ef4d607d24c105fc414ab0
PeterStAndrews <pm78@st-andrews.ac.uk>
2018-05-18 19:55:38 +0100

    Update hmf.py

--- network_processes/hmf.py
+++ network_processes/hmf.py
@@ -33,18 +33,55 @@ class HMF( NETWORK ):
     -------------
     .. [1] R. Pastor-Satorras and A. Vespignani. 'Epidemic spreading 
            in scale-free networks', Phys. Rev. Lett., vol. 86, pp. 3200-3203, 2001. '''
-
+    
+    def model( self, t, y, pInfect, pRecover, k, ave_k, Pk ):
+        '''Return functions for changes in states for the kth model.
+        :param pInfect: rate of infection
+        :param pRecover: rate of recovery
+        :returns: change functions'''
+            
+        def theta( ave_k, Pk, I_k ):
+            '''Function makes theta(t) for a non-correlated network.
+            :param ave_k: average degree
+            :param Pk: degree distribution
+            :param i: I_k value
+            :returns: theta(t)'''
+            summation = 0
+            for k in Pk.keys():
+                summation += (k - 1) * Pk[k] * I_k
+            return ( summation + 0.0 ) / ave_k 
+            
+        # unpack current state
+        S, I, R = y
+            
+        # define the update equations 
+        dS = - k * pInfect * S * theta(ave_k, Pk, I)
+        dI = k * pInfect * S * theta(ave_k, Pk, I) - pRecover * I
+        dR = pRecover * I
+            
+        return np.array([dS, dI, dR])
+    
+    def initialisation( self, params):
+        '''Initialises the kth state.
+        :param params: the experimental parameters
+        :returns array: the initial kth state'''
+        pInfected = params['pInfected']
+        return np.array([1 - pInfected, pInfected, 0], dtype=float)
+        
+    def param_vector( self, params, k, ave_k, Pk):
+        '''A vector of parameters required for the integrator.
+        :param params: the experimental parameters
+        :param k: the degree
+        :param ave_k: the average degree
+        :param Pk: the degree distribution'''
+        pInfect = params['pInfect']
+        pRecover = params['pRecover']
+        return pInfect, pRecover, k, ave_k, Pk
+    
     def do( self, params ):
         '''runs the experiment.'''
         rc = dict()
-        
-        pInfect = params['pInfect']
-        pRecover = params['pRecover']
-        pInfected = params['pInfected']
-        
-        S = dict()
-        I = dict()
-        R = dict()
+        states = dict()
         
         # grab a copy of the network
         g = self._network
@@ -54,57 +91,27 @@ class HMF( NETWORK ):
         
         # find the average degree of the network
         ave_k = self.average_degree(Pk)
-        
-        def theta( ave_k, Pk, I_k ):
-            '''Function makes theta(t) for a non-correlated network.
-            
-            :param ave_k: average degree
-            :param Pk: degree distribution
-            :param i: I_k value
-            :returns: theta(t)'''
-            summation = 0
-            for k in Pk.keys():
-                summation += (k - 1) * Pk[k] * I_k
-            return ( summation + 0.0 ) / ave_k 
-    
-        def make_sir( t, y, pInfect, pRecover, k, ave_k, Pk ):
-            '''Return functions for changes in the susceptible, infectious, and recovered
-            sub-populations for particular rates of recovery and infection.
-            
-            :param pInfect: rate of infection
-            :param pRecover: rate of recovery
-            :returns: change functions'''
-            
-            S, I, R = y
-            
-            dS = - k * pInfect * S * theta(ave_k, Pk, I)
-            dI = k * pInfect * S * theta(ave_k, Pk, I) - pRecover * I
-            dR = pRecover * I
-            
-            return [dS, dI, dR]
-        
+
         for k in Pk.keys():
             
             # initial conditions vector for kth system
-            y0 = [1 - pInfected, pInfected, 0]
+            y0 = self.initialisation(params)
+            
+            vec = self.param_vector(params, k, ave_k, Pk)
             t0 = 0
             
             t1 = 150 
             dt = 1
             
-            r = ode(make_sir).set_integrator('dopri5', method = 'adams')
-            r.set_initial_value(y0, t0).set_f_params(pInfect, pRecover, k, ave_k, Pk)       
+            r = ode(self.model).set_integrator('dopri5', method = 'adams')
+            r.set_initial_value(y0, t0).set_f_params(*vec)       
             
             while r.successful() and r.t < t1:
                 r.integrate(r.t+dt)
             
             # report final results for kth system
-            S[k] = r.y[0] * Pk[k]
-            I[k] = r.y[1] * Pk[k]
-            R[k] = r.y[2] * Pk[k]
+            states[k] = r.y * Pk[k]
         
-        rc['S_final'] = sum(S.values())
-        rc['I_final'] = sum(I.values())
-        rc['R_final'] = sum(R.values())
+        rc['final_state'] = sum(states.values())
         
         return rc
