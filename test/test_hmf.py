04327a02e3a504bc707d89c93497088897b92433
PeterStAndrews <pm78@st-andrews.ac.uk>
2018-09-07 15:41:45 +0100

    test suite

---
+++ test/test_hmf.py
@@ -0,0 +1,36 @@
+# test hmf for `Network-processes`
+#
+# Copyright (C) 2017 Peter Mann
+# 
+# This file is part of `Network_processes`, for epidemic network 
+# analytical results using Python.
+#
+# `Network_processes` is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# `Network_processes` is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with `Network_processes`. If not, see <http://www.gnu.org/licenses/gpl.html>.
+
+from network_processes import *
+import unittest
+
+class HMFTest(unittest.TestCase):
+	'''Tests for `HMF` class in `hmf.py`. ```
+
+
+	
+
+
+
+
+
+
+
+
